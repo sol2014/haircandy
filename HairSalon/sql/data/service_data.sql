73ace09d74882411f8737d0edd520d335a3cff61
draekz <draekz@6949e6a1-5956-0410-ab7a-655ecad5f349>
2008-11-09 19:15:02 +0000

    Added a TODO list to keep track of items we must fix or complete. Also fixed the services database so they are all 15 minute increments.

--- HairSalon/sql/data/service_data.sql
+++ HairSalon/sql/data/service_data.sql
@@ -1,22 +1,22 @@
 insert into service (name,description,duration,price,enabled) 
-values ('cuts','make hair short and clean',1,10.99,true);
+values ('cuts','make hair short and clean',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('color','color and highlight hair',2,10.99,true);
+values ('color','color and highlight hair',45,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('perm','make hair curly',3,10.99,true);
+values ('perm','make hair curly',90,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('makeup','the applasdf cosmetics',4,10.99,true);
+values ('makeup','the applasdf cosmetics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('asdsd','the application osdafsdfastics',4,10.99,true);
+values ('asdsd','the application osdafsdfastics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('makefdup','theasdfsdftion of cosmetics',4,10.99,true);
+values ('makefdup','theasdfsdftion of cosmetics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('masdfakeup','the applasdfasdetics',4,10.99,true);
+values ('masdfakeup','the applasdfasdetics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('mfakasdfeup','the applicasdff cosmetics',4,10.99,true);
+values ('mfakasdfeup','the applicasdff cosmetics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('masdfkeup','the application oasdfetics',4,10.99,true);
+values ('masdfkeup','the application oasdfetics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('makedfup','the asdfof cosmetics',4,10.99,true);
+values ('makedfup','the asdfof cosmetics',15,10.99,true);
 insert into service (name,description,duration,price,enabled) 
-values ('msdfakeup','the applicatasdf cosmetics',4,10.99,true);
+values ('msdfakeup','the applicatasdf cosmetics',15,10.99,true);

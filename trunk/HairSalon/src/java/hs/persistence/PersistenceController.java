0c1c4a96f2df31cb5a263693bb84e1d973def1e5
draekz <draekz@6949e6a1-5956-0410-ab7a-655ecad5f349>
2008-11-17 07:28:34 +0000

    Removed old connection pool code.

--- trunk/HairSalon/src/java/hs/persistence/PersistenceController.java
+++ trunk/HairSalon/src/java/hs/persistence/PersistenceController.java
@@ -9,7 +9,6 @@
  */
 package hs.persistence;
 
-import java.sql.*;
 import java.util.*;
 
 import hs.core.*;
@@ -23,128 +22,124 @@ import hs.persistence.brokers.*;
  * 
  * @author Philippe Durand
  */
-public class PersistenceController {
+public class PersistenceController
+{
+	private static Hashtable<Class, BrokerInterface> brokers = new Hashtable<Class, BrokerInterface> ();
 
-	private static Hashtable<Class, BrokerInterface> brokers = new Hashtable<Class, BrokerInterface>();
-	private static DataConnectionPool connectionPool;
+	public static void initialize (String url, String username, String password)
+	{
+		MultithreadedJDBCConnectionPool connectionPool = MultithreadedJDBCConnectionPool.getConnectionPool ("com.mysql.jdbc.Driver", url, username, password);
 
-	public static void initialize(String url, String username, String password) {
-		//DataConnectionPool connectionPool = DataConnectionPool.getInstance ();
-		//try
-		//{
-		//connectionPool.initialize (url, username, password);
-		//}
-		//catch (ClassNotFoundException cnfe)
-		//{
-		//LogController.write ("PersistenceController->ClassNotFoundException while initializing the manager");
-		//return;
-		//}
-		//catch (InstantiationException ie)
-		//{
-		//LogController.write ("PersistenceController->InstantiationException while initializing the manager");
-		//return;
-		//}
-		//catch (IllegalAccessException iae)
-		//{
-		//LogController.write ("PersistenceController->IllegalAccessException while initializing the manager");
-		//return;
-		//}
-		//catch (SQLException sqle)
-		//{
-		//LogController.write ("PersistenceController->SQLException while initializing the manager: " + sqle.getMessage ());
-		//return;
-		//}
+		LogController.write ("PersistenceController->Done initializing the database.");
 
-		MultithreadedJDBCConnectionPool connectionPool = MultithreadedJDBCConnectionPool.getConnectionPool("com.mysql.jdbc.Driver", url, username, password);
-
-		LogController.write("PersistenceController->Done initializing the database.");
-
-		try {
+		try
+		{
 			// Initialize all the brokers we use here.
-			brokers.put(Class.forName("hs.objects.AddressBean"), AddressBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.AppointmentBean"), AppointmentBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.AvailabilityExceptionBean"), AvailabilityExceptionBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.ClientBean"), ClientBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.EmployeeBean"), EmployeeBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.EmployeeHoursBean"), EmployeeHoursBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.ProductBean"), ProductBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.SaleBean"), SaleBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.SalonBean"), SalonBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.ScheduleBean"), ScheduleBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.ScheduleHoursBean"), ScheduleHoursBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.ScheduleExceptionBean"), ScheduleExceptionBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.ServiceBean"), ServiceBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.SupplierBean"), SupplierBroker.getInstance());
-			brokers.put(Class.forName("hs.objects.AlertBean"), AlertBroker.getInstance());
-		} catch (ClassNotFoundException ex) {
-			LogController.write("PersistenceController->Class not found: " + ex.getMessage());
+			brokers.put (Class.forName ("hs.objects.AddressBean"), AddressBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.AppointmentBean"), AppointmentBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.AvailabilityExceptionBean"), AvailabilityExceptionBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.ClientBean"), ClientBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.EmployeeBean"), EmployeeBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.EmployeeHoursBean"), EmployeeHoursBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.ProductBean"), ProductBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.SaleBean"), SaleBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.SalonBean"), SalonBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.ScheduleBean"), ScheduleBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.ScheduleHoursBean"), ScheduleHoursBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.ScheduleExceptionBean"), ScheduleExceptionBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.ServiceBean"), ServiceBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.SupplierBean"), SupplierBroker.getInstance ());
+			brokers.put (Class.forName ("hs.objects.AlertBean"), AlertBroker.getInstance ());
+		}
+		catch (ClassNotFoundException ex)
+		{
+			LogController.write ("PersistenceController->Class not found: " + ex.getMessage ());
 			return;
 		}
 
-		LogController.write("PersistenceController->Done registering the brokers.");
+		LogController.write ("PersistenceController->Done registering the brokers.");
 	}
 
-	public static void shutdown() {
-		if (connectionPool != null) {
-			connectionPool.closeConnections();
+	public static void shutdown ()
+	{
+		
+	}
 
-			connectionPool.destroy();
-
-			connectionPool = null;
+	private static BrokerInterface getBrokerForType (Class type) throws BrokerNotFoundException
+	{
+		if (brokers.containsKey (type))
+		{
+			return brokers.get (type);
+		}
+		else
+		{
+			String error = "PersistenceController->Unable to find broker with type: " + type.getCanonicalName ();
+			LogController.write (error);
+			throw new BrokerNotFoundException (error);
 		}
 	}
 
-	private static BrokerInterface getBrokerForType(Class type) throws BrokerNotFoundException {
-		if (brokers.containsKey(type)) {
-			return brokers.get(type);
-		} else {
-			String error = "PersistenceController->Unable to find broker with type: " + type.getCanonicalName();
-			LogController.write(error);
-			throw new BrokerNotFoundException(error);
+	public static DataBean load (DataBean data)
+	{
+		try
+		{
+			BrokerInterface broker = getBrokerForType (data.getClass ());
+			return broker.load (data);
 		}
-	}
-
-	public static DataBean load(DataBean data) {
-		try {
-			BrokerInterface broker = getBrokerForType(data.getClass());
-			return broker.load(data);
-		} catch (BrokerNotFoundException ex) {
+		catch (BrokerNotFoundException ex)
+		{
 			return null;
 		}
 	}
 
-	public static DataBean[] search(DataBean data) {
-		try {
-			BrokerInterface broker = getBrokerForType(data.getClass());
-			return broker.search(data);
-		} catch (BrokerNotFoundException ex) {
+	public static DataBean[] search (DataBean data)
+	{
+		try
+		{
+			BrokerInterface broker = getBrokerForType (data.getClass ());
+			return broker.search (data);
+		}
+		catch (BrokerNotFoundException ex)
+		{
 			return null;
 		}
 	}
 
-	public static boolean exists(DataBean data) {
-		try {
-			BrokerInterface broker = getBrokerForType(data.getClass());
-			return broker.exists(data);
-		} catch (BrokerNotFoundException ex) {
+	public static boolean exists (DataBean data)
+	{
+		try
+		{
+			BrokerInterface broker = getBrokerForType (data.getClass ());
+			return broker.exists (data);
+		}
+		catch (BrokerNotFoundException ex)
+		{
 			return false;
 		}
 	}
 
-	public static boolean commit(DataBean data) {
-		try {
-			BrokerInterface broker = getBrokerForType(data.getClass());
-			return broker.commit(data);
-		} catch (BrokerNotFoundException ex) {
+	public static boolean commit (DataBean data)
+	{
+		try
+		{
+			BrokerInterface broker = getBrokerForType (data.getClass ());
+			return broker.commit (data);
+		}
+		catch (BrokerNotFoundException ex)
+		{
 			return false;
 		}
 	}
 
-	public static boolean delete(DataBean data) {
-		try {
-			BrokerInterface broker = getBrokerForType(data.getClass());
-			return broker.delete(data);
-		} catch (BrokerNotFoundException ex) {
+	public static boolean delete (DataBean data)
+	{
+		try
+		{
+			BrokerInterface broker = getBrokerForType (data.getClass ());
+			return broker.delete (data);
+		}
+		catch (BrokerNotFoundException ex)
+		{
 			return false;
 		}
 	}

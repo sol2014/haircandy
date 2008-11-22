deaf899ed72f6e042acc54af233fe3c30b15518b
HoraceLSWan <HoraceLSWan@6949e6a1-5956-0410-ab7a-655ecad5f349>
2008-11-22 14:50:19 +0000

    Add new rows into the tables.

--- trunk/HairSalon/sql/data/client_data.sql
+++ trunk/HairSalon/sql/data/client_data.sql
@@ -1,2 +1,10 @@
 insert into client (first_name, last_name, address_no, phone_number,enabled)
 values ('Fair','Lady', 1, '4035551234',true);
+
+insert into client (first_name, last_name, address_no, phone_number,enabled)
+values ('Raymond','Mah', 2, '4037082234',true);
+insert into client (first_name, last_name, address_no, phone_number,enabled)
+values ('Mary','Jordon', 5, '4037122672',true);
+insert into client (first_name, last_name, address_no, phone_number,enabled)
+values ('Norm','Scott', 6, '4038211500',true);
+
---
+++ trunk/HairSalon/sql/data/sale_data.sql
@@ -0,0 +1,18 @@
+
+insert into sale (client_no, employee_no, payment_type, total_due, total_tax, discount, payment, is_complete)
+values (1, 4, 'Cash', 100, 5, 0, 105, true);
+
+insert into sale (client_no, employee_no, payment_type, total_due, total_tax, discount, payment, is_complete)
+values (2, 3, 'Credit', 20, 1, 0, 21, true);
+
+insert into sale (client_no, employee_no, payment_type, total_due, total_tax, discount, payment, is_complete)
+values (1, 3, 'Credit', 100, 5, 0, 105, true);
+
+insert into sale (client_no, employee_no, payment_type, total_due, total_tax, discount, payment, is_complete)
+values (2, 3, 'Credit', 40, 2, 10, 38, true);
+
+insert into sale (client_no, employee_no, payment_type, total_due, total_tax, discount, payment, is_complete)
+values (4, 5, 'Debit', 50, 2.5, 0, 52.5, true);
+
+insert into sale (client_no, employee_no, payment_type, total_due, total_tax, discount, payment, is_complete)
+values (4, 5, 'Cheque', 10, 1, 100, 0, true);
